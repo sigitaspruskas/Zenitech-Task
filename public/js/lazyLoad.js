e2876696c5e5f12c881837f14702b629bb9512ad
Sigitas <sigitaspruskas@gmail.com>
2020-01-10 14:52:08 +0200

    v2: added working sorting

--- public/js/lazyLoad.js
+++ public/js/lazyLoad.js
@@ -5,7 +5,8 @@ class LazyLoad {
 
     key = '00ac5f70d662304b87e7da585bbdef9d';
     pageCounter = 1;
-    BASE_URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=nyc&per_page=20&page=${this.pageCounter}&format=json&nojsoncallback=1`;
+    BASE_URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=nyc&per_page=5&page=${this.pageCounter}&format=json&nojsoncallback=1`;
+    db = [];
 
     /**
      * sets default variables and their values when class object is created.
@@ -15,7 +16,9 @@ class LazyLoad {
         this.container = document.querySelector(".load-more-container");
         this.leftContainer = document.querySelector('.left-side');
         this.previewContainer = document.querySelector('.preview-container');
-        this.db = [];
+        this.countContainer = document.querySelector('.img-count');
+        this.btn = document.querySelector('.sort-btn');
+        this.btn.addEventListener('click', () => this.sort(this.db));
     };
 
     /**
@@ -23,67 +26,103 @@ class LazyLoad {
      * @param {number} pageNumber this must be a page number, that will be replaced in this.BASE_URL.
      */
     urlHandler(pageNumber) {
+        console.log(`=> Initialize urlHandler()... (updating BASE_URL).`);
         this.pageCounter = ++pageNumber;
-        return this.BASE_URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=nyc&per_page=20&page=${this.pageCounter}&format=json&nojsoncallback=1`;
+        return this.BASE_URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=nyc&per_page=5&page=${this.pageCounter}&format=json&nojsoncallback=1`;
     };
 
     /**
-     * This method initializes async requestApi() function, that fetches the data from API URL, and calls render() function, to render images thumbnails.
+     * This method sorts this.db alphabetically, and then calls load() to load sorted this.db, and render it.
+     * @param {*} database 
      */
-    load() {
-        const requestApi = async (url) => {
-            const response = await fetch(url);
-            const json = await response.json();
-            console.log('');
-            console.log(`Initialize load()... Array of ${json.photos.photo.length} photo objects Loaded.`);
-            this.render(json.photos.photo, json.photos);
-        };
+    sort(database) {
+        console.log(`=> Initialize sort()... db to sort:`);
 
-        requestApi(this.BASE_URL);
+        let titleArray = [];
+        let sortedArray = [];
+
+        database.forEach(photoObject => {
+            titleArray.push(photoObject.title);
+        });
+
+        titleArray.sort();
+        titleArray.filter(title => {
+            this.db.forEach(photoObj => {
+                if (title == photoObj.title) {
+                    sortedArray.push(photoObj);
+                }
+            })
+        });
+
+        this.btn.innerHTML = 'SORTED';
+        this.load(sortedArray);
     }
 
     /**
-     * This method takes in array of objects, and response object from fetch(). It takes 5 photo objects in one iteration, and renders them.
-     * If the last photos from array were rendered, it calls urlHandler() function, to update API URL, and resets this.startCounter to 0.
-     * this.startCounter is used for splicing temporary array of 5 photos, from the original photoArray.
-     * @param {array} photoArray Array of photo objects.
-     * @param {*} responseObj must be response object that contains data and objects array from fetch, ex. 'json.photos'.
+     * This methods initializes requestApi() function, that fetches the data from API URL 
+     * (or loads existing sorted this.db if it was called from sort() method), to render images thumbnails.
      */
-    render(photoArray, responseObj) {
-        console.log(responseObj);
+    async load(dbArray) {
+        console.log(`=> Initialize load()... this.db at the start of load():`);
 
-        let start, end, arr;
-        let picsLeft = photoArray.length - this.startCounter;
+        let tempDbArray = this.db;
+        let fetchedArray = [];
 
-        if (picsLeft >= 5) {
-            start = this.startCounter;
-            end = start + 5;
-            arr = photoArray.slice(start, end);
-            console.log(`Loaded photos from API array: ${arr.length}.`);
+        if (dbArray == null) {
+            this.btn.innerHTML = 'SORT ALPHABETICALLY';
+
+            const requestApi = async (url) => {
+                console.log('=> Initialize requiestAPI()... (fetch data).');
+                const response = await fetch(url);
+                const json = await response.json();
+                return json.photos.photo;
+            };
+
+            fetchedArray = await requestApi(this.BASE_URL);
+            this.db = await tempDbArray.concat(fetchedArray);
+            await this.render(fetchedArray);
+
         } else {
-            start = photoArray.length - picsLeft;
-            end = null;
-            arr = photoArray.slice(start);
-            console.log(`Loaded photos from API array: ${arr.length}.`);
-            this.urlHandler(this.pageCounter);
-            return this.startCounter = 0;
-        };
+            this.previewContainer.innerHTML = '';
+            this.db = dbArray;
+            this.render(dbArray);
+        }
 
-        this.db = this.db.concat(arr);
+        return this.db;
+    }
 
-        if (arr.length > 0) {
+    /**
+     * This method takes in array of objects to be renderred in HTML. It then calls urlHandler() function, to update API URL.
+     * @param {array} photoArray Array of photo objects.
+     */
+    render(photoArray) {
+        if (photoArray.length > 0) {
+            console.log(`=> Initialize render()... Loaded photos to be rendered from API array: ${photoArray.length}.`);
+
+            this.countContainer.innerHTML = '';
+            this.countContainer.innerHTML = `IMAGES LOADED: ${this.db.length} `
+
             let images = '';
-            arr.forEach(photoObj => {
+
+            photoArray.forEach(photoObj => {
                 images += `
-                    <div class="preview-box" id="${photoObj.id}">
-                        <img src="https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${photoObj.secret}.jpg" alt="${photoObj.title}" class="preview-box-img">
-                    </div>
+                <div class="img-info-container">
+                <div class="preview-box" id="${photoObj.id}">
+                <img src="https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${photoObj.secret}.jpg" alt="${photoObj.title}" class="preview-box-img">
+                </div>
+                <div class="author-text">
+                <div class="author-title">Title:
+                <div class="title-text">${photoObj.title}</div>
+                </div>
+                </div>
+                </div>
                 `;
             })
+
             this.previewContainer.insertAdjacentHTML('beforeend', images);
-            this.container.dataset.index = end;
         }
-        this.startCounter = end;
+
+        this.urlHandler(this.pageCounter);
     };
 
     /**
--- readme.md
+++ readme.md
@@ -3,15 +3,16 @@
 behaving as follows:*
 1. Front end, which offers two columns. Image thumbnails down the left hand side and the title of the image down the right hand side.
 2. The app provides an infinite scroll experience.
-3. Back-end component, written as a node module, which performs the fetching of images. Keep my API key out of the public.
+3. Back-end component, written as a node module, which performs the fetching of images. Keeps my API key out of the public.
 4. The app holds an open connection to the server, taking incremental updates (i.e. no poll).
+5. Scroll bar on thumbnail section is hidden.
 
 ### How to use node server and npm:
 **Start a server:** 
 1. Download or clone the project.
 2. In terminal, type 'npm install' - installs all the node module packages that are listed package.json;
 3. nodemon is installed: in terminal type 'npm run dev' -> node module runs "dev" script that launches nodemon;
-4. The ersver is running on 5000 port, access it via http://localhost:5000;
+4. The server is running on 5000 port, access it via http://localhost:5000;
 5. **stop server : 'ctrl + C'**
 
 **Tips:**
